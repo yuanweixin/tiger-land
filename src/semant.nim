75cbf2d17d55458fb9b2df328fd3540a94314eec
Wei Xin Yuan <weixinyuan6@gmail.com>
2023-01-29 12:09:47 +0000

    wip

--- src/semant.nim
+++ src/semant.nim
@@ -3,9 +3,9 @@ import options
 import symbol
 import translate
 
-{.experimental: "implicitDeref".}
-
 var typeTagCounter = 0
+
+# A new tag is assigned to each array/record declaration.
 type TypeTag = int
 
 proc newTypeTag(): int =
@@ -21,17 +21,18 @@ type TypeKind* = enum
     UnitT
     NameT
 
+# This is the type representation used by the type checker.
 type Type* = object
     case kind*: TypeKind
     of RecordT:
-        symTy*: seq[(Symbol, Ty)]
+        symTy*: seq[(Symbol, Type)]
         rtt*: TypeTag
     of ArrayT:
-        ty*: Ty
+        ty*: Type
         att*: TypeTag
     of NameT:
         s*: Symbol
-        tyopt*: Option[Ty]
+        tyopt*: Option[Type]
     of NilT, IntT, StringT, UnitT:
         discard
 
@@ -43,6 +44,7 @@ type
         case kind*: EnvEntryKind
         of VarEntry:
             ty*: Type
+            assignable*: bool = true
         of FunEntry:
             formals*: seq[Type]
             result*: Type
@@ -56,7 +58,7 @@ type ExpTy = (TranslatedExp, Type)
 var baseTEnv: var TEnv
 var baseVEnv: var VEnv
 
-proc error (hasErr: var bool, pos: int, msg: string) =
+proc error (hasErr: var bool, pos: int, msg: varargs[string, `$`]) =
     hasErr = true
     echo pos, " ", msg
 
@@ -91,72 +93,6 @@ proc transExp*(hasErr: var bool, venv: var VEnv, tenv: var TEnv,
                 if (lk, rk) notin [(IntT, IntT), (ArrayT, ArrayT), (RecordT, RecordT)]:
                     error hasErr, e.opos, "comparison only supported for integer, array or record types"
             return (42, Type(kind: IntT))
-    of NilExp:
-        return (42, Type(kind: NilT))
-    of IntExp:
-        return (42, Type(kind: IntT))
-    of VarExp:
-        return "TODO"
-    of StringExp:
-        return (42, Type(kind: StringT))
-    of CallExp:
-        return "TODO"
-    of RecordExp:
-        return "TODO"
-    of SeqExp:
-        return "TODO"
-    of AssignExp:
-        return "TODO"
-    of IfExp:
-        let (_, tytest) = transExp(hasErr, venv, tenv, e.iftest)
-        if tytest.kind != IntT:
-            error hasErr, e.ifpos, "if condition must be of type integer"
-        let (_, tythen) = transExp(hasErr, venv, tenv, e.then)
-        if e.els.isNone:
-            if tythen.kind != UnitT:
-                error hasErr, e.ifpos, "if-then branch must not produce a value"
-        else:
-            let (_, tyelse) = transExp(hasErr, venv, tenv, e.els.get)
-            if tythen.kind != tyelse.kind:
-                error hasErr, e.ifpos, "if-then-else branches must have same type"
-        # if type error happens with branches, will just return the then.
-        return (42, tythen)
-    of WhileExp:
-        let (_, tytest) = transExp(hasErr, venv, tenv, e.wtest)
-        if tytest.kind != IntT:
-            error hasErr, e.wpos, "while condition must be of type integer"
-        let (_, tybody) = transExp(hasErr, venv, tenv, e.wbody)
-        if tybody.kind != UnitT:
-            error hasErr, e.wpos, "while body must not produce a value"
-        return (42, Type(kind: UnitT))
-    of ForExp:
-        let (_, tylo) = transExp(hasErr, venv, tenv, e.lo)
-        let (_, tyhi) = transExp(hasErr, venv, tenv, e.hi)
-        if tylo.kind != IntT or tyhi.kind != IntT:
-            error hasErr, e.fpos, "values of for range must be integer"
-        else:
-            venv.beginScope()
-            venv.enter(e.fvar, EnvEntry(kind: VarEntry, ty: Type(kind: IntT)))
-            let (_, tyfbody) = transExp(hasErr, venv, tenv, e.fbody)
-            if tyfbody.kind != UnitT:
-                error hasErr, e.fpos, "for body must not produce a value"
-            venv.endScope()
-        return (42, Type(kind: UnitT))
-    of BreakExp:
-        # breaks need to be in context of a while or for, not crossing a procedure
-        # call boundary, i.e. if p calls q and break is in q, does not affect p.
-        # TODO fix this in part b.
-        return (42, Type(kind: UnitT))
-    of LetExp:
-        return "TODO"
-    of ArrayExp: # Id Lbrack exp Rbrack Of exp:
-        let aty = tenv.look e.arrtyp
-        if aty.isNone:
-            error hasErr, e.arrpos, e.arrtyp.name & " is undefined."
-        elif ty.get.kind != ArrayT:
-            error hasErr, e.arrpos, e.arrtyp.name & " is not array type!"
-
-        return "TODO"
 
 proc transDec*(hasErr: var bool, venv: var VEnv, tenv: var TEnv, d: absyn.Dec) =
     discard
