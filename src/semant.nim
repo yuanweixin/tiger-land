f2bbecc8acf302a5b39dfd62391ac8eb4f74c3ae
Wei Xin Yuan <weixinyuan6@gmail.com>
2023-01-29 14:40:31 +0000

    wip

--- src/semant.nim
+++ src/semant.nim
@@ -20,11 +20,13 @@ type TypeKind* = enum
     ArrayT
     UnitT
     NameT
+    ErrorT # returned when type checking fails
 
-# This is the type representation used by the type checker.
+           # This is the type representation used by the type checker.
 type Type* = object
     case kind*: TypeKind
     of RecordT:
+        name*: string
         symTy*: seq[(Symbol, Type)]
         rtt*: TypeTag
     of ArrayT:
@@ -64,7 +66,39 @@ proc error (hasErr: var bool, pos: int, msg: varargs[string, `$`]) =
 
 proc transVar*(hasErr: var bool, venv: var VEnv, tenv: var TEnv,
         v: absyn.VarR): ExpTy =
-    discard
+    case v.kind
+    of SimpleVar:
+        let tyOpt = venv.look v.svs
+        if tyOpt.isNone:
+            error hasErr, v.svp, "use of undeclared variable."
+            raise newException(Exception, "todo return some error type")
+        return (42, tyOpt.get)
+    of FieldVar: # id.id
+        let (_, tyLhs) = transVar(hasErr, venv, tenv, v.fvar)
+        if tyLhs.kind != RecordT:
+            error hasErr, v.fvp, "tried to access field of a non-record type"
+        else:
+            var found = False
+            for sym, symty in tyLhs.symTy:
+                if sym == v.fvs:
+                    return (42, symty)
+            error hasErr, v.fvp, "Field ", v.fvs.name,
+                    " is not part of record ", tyLhs.name
+        raise newException(Exception, "todo return error type")
+    of SubscriptVar:
+        let (_, tyLhs) = transVar(hasErr, venv, tenv, v.subvar)
+        if tyLhs.kind != ArrayT:
+            error hasErr, v.pos, "tried to access a non-array"
+        else:
+            let (_, tyExp) = transExp(hasErr, venv, tenv, v.exp)
+            if tyExp.kind != IntT:
+                error hasErr, v.pos, "array index is not an integer!"
+            else:
+                return (42, tyLhs.ty)
+        raise newException(Exception, "todo return err type")
+
+## TODO gonna write it with global error var, and pretend this is first site of type
+## error, and see what happens.
 
 proc transExp*(hasErr: var bool, venv: var VEnv, tenv: var TEnv,
         e: absyn.Exp): ExpTy =
@@ -102,12 +136,14 @@ proc transExp*(hasErr: var bool, venv: var VEnv, tenv: var TEnv,
         return (42, tyv)
     of StringExp:
         return (42, Type(kind: StringT))
-    # of CallExp:
+    of CallExp:
         let fentryOpt = tenv.look e.fun
         if fentryOpt.isNone:
             error hasErr, e.cp, "Trying to call an undeclared function ", e.fun.name
+
         elif fentryOpt.get.kind != FunEntry:
             error hasErr, e.cp, e.fun.name, " is not a function!"
+        else:
             if e.args.len != fentry.get.formals.len:
                 error hasErr, e.cp, e.fun.name, " has ", fentry.get.formals.len,
                         " arguments but is called with ", e.args.len
@@ -126,15 +162,35 @@ proc transExp*(hasErr: var bool, venv: var VEnv, tenv: var TEnv,
                 # venv.enter formal.s, EnvEntry(kind: VarEntry, ty: formal.tyopt.get)
             # venv.endScope()
             return (42, fentryOpt.get.result)
-    of FunEntry:
-        # TODO
-        return (42, Type(kind: StringT))
     of RecordExp:
-        # TODO
-        return (42, Type(kind: StringT))
+        let recEntryOpt = tenv.look e.rectyp
+        if recEntryOpt.isNone:
+            error hasError, e.rpos, e.rectyp.name, " has not been declared"
+        elif recEntryOpt.get.kind != VarEntry or recEntryOpt.get.ty.kind != RecordT:
+            error hasError, e.rpos, e.rectyp.name, " is not a record type"
+        else:
+            let expRecTy = recEntryOpt.get.ty
+            if expRecTy.symTy.len != e.fields.len:
+                error hasError, e.rpos, e.rectyp.name, " does not have same fields as the ones given "
+            else:
+                for sym, exp, pos in e.fields:
+                    for symInTy, typExpected in expRecTy.symTy:
+                        if sym == symInTy:
+                            let (_, tyActual) = transExp(hasErr, venv, tenv, exp)
+                            if typExpected != tyActual:
+                                error hasError, pos, sym.name,
+                                        " is expected to have type ",
+                                        tyExpected, " but got ", tyActual
+            return (42, expRecTy)
+        raise newException(Exception, "todo handle when can't return a type")
     of SeqExp:
-        # TODO
-        return (42, Type(kind: StringT))
+        var i = 0
+        while i < e.eplist.len:
+            let (_, tyExp) = transExp(hasErr, venv, tenv, exp)
+            inc i
+            if i == e.eplist.len:
+                return (42, tyExp)
+        return (42, Type(kind: UnitT))
     of AssignExp:
         # nil can be assigned to record type vars
         let (_, lhs) = transVar(hasErr, venv, tenv, e.avar)
@@ -162,7 +218,7 @@ proc transExp*(hasErr: var bool, venv: var VEnv, tenv: var TEnv,
         return (42, tythen)
     of WhileExp:
         let (_, tytest) = transExp(hasErr, venv, tenv, e.wtest)
-        if tytest.kind != IntT:
+        if tytest.kcalind != IntT:
             error hasErr, e.wpos, "while condition must be of type integer"
         let (_, tybody) = transExp(hasErr, venv, tenv, e.wbody)
         if tybody.kind != UnitT:
