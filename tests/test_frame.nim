2254c4fe0e8bde5b87f1b048cd5099ad3e28e58d
Wei Xin Yuan <weixinyuan6@gmail.com>
2023-02-10 14:09:04 +0000

    update frame test

--- tests/test_frame.nim
+++ tests/test_frame.nim
@@ -1,8 +1,9 @@
+import unittest
 import translate
-import frame
 import ../src/frame/x86_ia64
 import ../src/frame/x86_ia32
 
-# this would crap out if the thing does not fit the concept. 
-type T1* = Translate[X86IA64Frame]
-type T2* = Translate[X86IA32Frame]
+test "Frame concept check":
+    doAssert X86IA64Frame is Frame
+    doAssert X86IA32Frame is Frame
+
